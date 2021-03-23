254d7772034195ee353b3ef7b75d6adce81d9670
kalkikotha <kalki6309@gmail.com>
2021-03-23 08:03:12 +0530

    Update product.js

--- src/product.js
+++ src/product.js
@@ -3,10 +3,10 @@ import React from "react";
 function Product(props) {
   return <div class="col-lg-4 col-md-6 mb-4">
     <div class="card h-100">
-      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></img></a>
+      <img class="card-img-top" src="http://placehold.it/700x400" alt=""></img>
       <div class="card-body">
         <h4 class="card-title">
-          <a href="#">{props.data.productname}</a>
+          {props.data.productname}
         </h4>
         <h5>${props.data.price}</h5>
         <p class="card-text">{props.data.description}</p>
